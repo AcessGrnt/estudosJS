29ac8dffb7d989c7fc195b95e363afa7ada1a32c
João Pedro Fagundes <70774421+AcessDnd@users.noreply.github.com>
2022-10-17 19:04:46 -0300

    Add files via upload

---
+++ varDificil/desafio.js
@@ -0,0 +1,8 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+const resposta = [varA, varB, varC] = [varB, varC, varA]; // ou const resposta = [varA = "B", varB = "C", varC = "A"];
+
+console.log(resposta)
+
