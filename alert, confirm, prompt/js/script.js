61e0d61264f1044e4249df9b004d449b5f1de5cf
João Pedro Fagundes <70774421+AcessDnd@users.noreply.github.com>
2022-10-17 18:30:35 -0300

    Add files via upload

---
+++ alert, confirm, prompt/js/script.js
@@ -0,0 +1,14 @@
+alert('Mensagem'); //Aparece um alerta!
+window.alert('Mensagem2');
+
+confirm('Deseja apagar?'); // false ou true
+window.confirm('Quer mesmo?');
+
+prompt('Olá'); // caixa de texto
+window.prompt('Diga seu nome:');
+
+let num1 = prompt('Digite um numero!');
+let num2 = prompt('Agora outro');
+
+
+
---
+++ alert, confirm, prompt/testes e etudos/js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite um numero:');
+let num2 = prompt('Digite outro:');
+
+num1 = parseFloat(num1);
+num1 = Number(num1);
+num2 = parseFloat(num2);
+num2 = Number(num2);
+
+window.alert(`A soma dos dois numeros digitados é: ${num1 + num2}`);
